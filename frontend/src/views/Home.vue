f8b25ee36f4c236715ee628f8174ad0ddd403ee1
aomsk <plaaom39@gmail.com>
2022-04-15 20:47:59 +0700

    feat: see post detail by postId

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -5,12 +5,14 @@
                 <div class="card mt-5">
                     <div class="card-body">
                         <h4 class="card-title">{{ post.title }}</h4>
-                        <button class="btn btn-dark" @click="testClick">อ่านต่อ</button>
+                        <!-- <h6>{{ post.postId }}</h6> -->
+                        <router-link v-bind:to="'/post/' + post.postId">
+                            <button class="btn btn-dark">อ่านต่อ</button>
+                        </router-link>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="text-primary">{{ posts }}</div>
     </div>
 </template>
 
@@ -28,48 +30,12 @@ export default {
         axios.get('http://howtocrud-env.eba-p33xseme.us-east-1.elasticbeanstalk.com/posts/all')
             .then(response => {
                 this.posts = response.data
-                console.log('response.data: ', response.data);
             })
             .catch(error => {
                 console.log(error)
             })
-        // axios
-        //     .get('http://howtocrud-env.eba-p33xseme.us-east-1.elasticbeanstalk.com/posts/all', {
-        //         params: {
-        //             productId: '1'
-        //         }
-        //     })
-        //     .then(response => {
-        //         this.one_products = response.data
-        //     })
-        //     .catch(error => {
-        //         console.log(error)
-        //     })
     },
     methods: {
-        getData() {
-            // axios.get('http://tutorialnodejs-env.eba-mrvzp3m5.us-east-1.elasticbeanstalk.com/product/all')
-            // .then(response => {
-            //     this.products = response.data.products
-            // })
-            // . catch(error => {
-            //     console.log(error)
-            // })
-        },
-        testClick() {
-            console.log('testClick')
-            // const id = this.posts.map((post) => {
-            //     return post.postId
-            // })
-
-            for (let i = 0; this.posts.length > i; i++) {
-                this.id = this.posts[i].postId
-                console.log(this.posts[i].postId)
-            }
-            // this.$router.push('/post/2')
-            const id = this.id
-            this.$router.push({ name: 'PostDetail', params: { id } }) // -> /user/eduardo
-        }
     }
 }
 </script>
