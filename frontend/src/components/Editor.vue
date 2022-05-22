59b4d11ca4ea8997c18c7d9da5b4547562a52d8e
aomsk <plaaom39@gmail.com>
2022-05-23 00:20:28 +0700

    feat: try to upload video

--- frontend/src/components/Editor.vue
+++ frontend/src/components/Editor.vue
@@ -20,7 +20,6 @@
             id="button_editor"
             @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
             :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
-            
             class="btn btn-outline-secondary m-1"
         >
             <font-awesome-icon icon="heading" />
@@ -31,7 +30,6 @@
             id="button_editor"
             @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
             :class="{ 'is-active': editor.isActive('heading', { level: 2 })}"
-            
             class="btn btn-outline-secondary m-1"
         >
             <font-awesome-icon icon="heading" />
@@ -41,7 +39,7 @@
             id="button_editor"
             @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
             :class="{ 'is-active': editor.isActive('heading', { level: 3 })}"
-            v-on:click ="clickedH3 = !clickedH3"
+            v-on:click="clickedH3 = !clickedH3"
             class="btn btn-outline-secondary m-1"
         >
             <font-awesome-icon icon="heading" />
@@ -99,7 +97,20 @@
         >
             <font-awesome-icon icon="image" />
         </button>
-        <button id="button_editor" @click="editor.chain().focus().setHorizontalRule().run()" class="btn btn-outline-secondary m-1">
+        <!-- <button 
+            id="button_editor"
+            class="btn btn-outline-secondary m-1"
+            type="button"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal2"
+        >
+            add iframe
+        </button> -->
+        <button
+            id="button_editor"
+            @click="editor.chain().focus().setHorizontalRule().run()"
+            class="btn btn-outline-secondary m-1"
+        >
             <font-awesome-icon icon="ruler-horizontal" />
         </button>
         <!-- <button @click="editor.chain().focus().setHardBreak().run()">hard break</button> -->
@@ -132,20 +143,68 @@
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" id="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" id="button" class="btn btn-outline-primary" @click="onUpload()">Upload Image</button>
+                        <button
+                            type="button"
+                            id="button"
+                            class="btn btn-outline-secondary"
+                            data-bs-dismiss="modal"
+                        >Close</button>
+                        <button
+                            type="button"
+                            id="button"
+                            class="btn btn-outline-primary"
+                            @click="onUpload()"
+                        >Upload Image</button>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- <img v-bind:src="image" /> -->
+        <!-- Modal Video -->
+        <!-- <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Upload Video</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <input
+                                type="file"
+                                class="form-control"
+                                aria-label="file example"
+                                accept="video/*"
+                                @change="onFileVideoSelected"
+                                required
+                            />
+                            <div class="invalid-feedback">Example invalid form file feedback</div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button
+                            type="button"
+                            id="button"
+                            class="btn btn-outline-secondary"
+                            data-bs-dismiss="modal"
+                        >Close</button>
+                        <button
+                            type="button"
+                            id="button"
+                            class="btn btn-outline-primary"
+                            @click="onUploadVideo()"
+                        >Upload Video</button>
+                    </div>
+                </div>
+            </div>
+        </div> -->
     </div>
-    <editor-content :editor="editor" class="mt-2 mb-3"/>
+    <editor-content :editor="editor" class="mt-2 mb-3" />
 </template>
 
 <script>
-import { Editor, EditorContent, BubbleMenu } from '@tiptap/vue-3'
+import { Editor, EditorContent } from '@tiptap/vue-3'
 import StarterKit from '@tiptap/starter-kit'
+// import iframe from './Iframe'
 import axios from 'axios'
 import Swal from 'sweetalert2'
 import Image from '@tiptap/extension-image'
@@ -164,9 +223,12 @@ export default {
         return {
             editor: null,
             selectedFile: null,
+            selectedVideoFile: null,
             image: '',
             imageName: '',
             listImages: [],
+            video: '',
+            videoName: ''
         }
     },
     watch: {
@@ -197,6 +259,52 @@ export default {
         this.editor.destroy()
     },
     methods: {
+        // async onUploadVideo() {
+        //     if (this.selectedVideoFile == null) {
+        //         Swal.fire({
+        //             title: 'กรุณาเลือกวิดีโอ',
+        //             icon: 'warning',
+        //             // confirmButtonText: 'OK'
+        //             showConfirmButton: false,
+        //             timer: 1500
+        //         })
+        //     } else {
+        //         const config = { headers: { 'Content-Type': 'multipart/form-data' } }
+        //         let fd = new FormData()
+        //         fd.append('file', this.selectedVideoFile)
+        //         await axios
+        //             .post('http://uploadimage-env.eba-xmegyc33.us-east-1.elasticbeanstalk.com/upload-image-post', fd, config)
+        //             // .post('http://localhost:3002/upload-image-post', fd, config)
+        //             .then(response => {
+        //                 console.log(response.data)
+        //                 this.video = response.data.location
+        //                 this.videoName = response.data.originalname
+        //                 this.selectedVideoFile = null
+        //                 Swal.fire({
+        //                     title: 'อัปโหลดวิดีโอสำเร็จ',
+        //                     icon: 'success',
+        //                     // confirmButtonText: 'OK'
+        //                     showConfirmButton: false,
+        //                     timer: 1500
+        //                 })
+        //             })
+        //             .catch(error => {
+        //                 console.log(error)
+        //             })
+        //         if (this.video) {
+        //             this.listImages.push(this.imageName)
+        //             // this.editor.chain().focus().setImage({ src: this.image }).run()
+        //             this.editor.chain().focus().setIframe({ src: 'https://how-to-post-images.s3.amazonaws.com/2022-05-20%2009-03-49.mp4' }).run()
+        //         }
+        //         this.$store.commit('setListImage', this.listImages)
+        //         console.log('this.listImages: ', this.listImages);
+        //         console.log('this.$store.state.list_image : ', this.$store.state.list_image);
+        //     }
+        // },
+        // onFileVideoSelected(event) {
+        //     this.selectedVideoFile = event.target.files[0]
+        //     console.log(this.selectedVideoFile)
+        // },
         onFileSelected(event) {
             this.selectedFile = event.target.files[0]
             console.log(this.selectedFile)
